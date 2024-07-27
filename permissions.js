450503968a2d2081f5d17a8f9319a58de32f37ab
sekolahanakindonesia <rizal.fahmi@sai-edu.org>
2024-07-27 08:21:15 +0700

    Update permissions.js

--- permissions.js
+++ permissions.js
@@ -1,19 +1,6 @@
-// Enkripsi data
-const codes = CryptoJS.AES.encrypt(
-  JSON.stringify({
-    "/kuli/sd.html": "sd",
-    "/kuli/smp.html": "smp",
-    "/kuli/sma.html": "sma",
-    "/kuli/tdu.html": "tdu",
-  }),
-  'your-secret-key'
-).toString();
-
-console.log("Encrypted Permissions:", codes);
-
-// Dekripsi data
-const bytes = CryptoJS.AES.decrypt(codes, 'your-secret-key');
-const accessPermissions = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-
-console.log("Decrypted Permissions:", accessPermissions);
-
+const codes = {
+  klsd10: "sd",
+  klsmp29: "smp",
+  klsma38: "sma",
+  kltdu47: "tdu",
+};
